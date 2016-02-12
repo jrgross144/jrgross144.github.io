b54fff9f4c331ecb4f732aa1522d5fc5bd2b0b35
Jacob Gross <grossj@msoe.edu>
2016-02-12 13:01:24 -0600

    added navigation buttons

--- search_website.js
+++ search_website.js
@@ -4,44 +4,80 @@ function execSearch(){
   
   var search = document.getElementById("search").value;
   if(search){
-	if(data){
-	  var temp = document.getElementById("results");
-	  while (temp.firstChild) {
-        temp.removeChild(temp.firstChild);
-	  }
-	}
-	$.ajax({
-      dataType: "json",
-      url: "https://api.twitch.tv/kraken/search/streams?q="+search,
-      success: display,
-      fail: error
-    });
+	
+    var url = "https://api.twitch.tv/kraken/search/streams?q="+search;
+	ajaxCall(url);
   }else{
     document.getElementById("test").innerHTML += "Not A Valid Search";
   }
 }
+
+function ajaxCall(url){
+	if(data){
+	  clean(document.getElementById("results"));
+	  clean(document.getElementById("navigation"));
+	}
+	$.ajax({
+      dataType: "json",
+      url: url,
+      success: display,
+      fail: error
+    });
+}
+
+//displays the results of the search
 function display(result){
   data = result;
   document.getElementById("count").innerHTML += data._total;
+  setUpNav(data._links);
   for(var i=0; i<data.streams.length; i++){
 	var stream = data.streams[i];
 	var row = document.createElement("tr");
 	var elem = document.createElement("td");
 	elem.innerHTML += "<br><br><h2>"+stream.channel.game+"</h2>"
+	
 	var img = document.createElement("img");
 	img.src = stream.preview.medium;
 	elem.appendChild(img);
+	
 	elem.innerHTML += "<br>"+stream.channel.status+"<br>";
+	
 	var link = document.createElement("a");
 	link.href = stream.channel.url;
 	link.text = stream.channel.display_name;
 	elem.appendChild(link);
+	
 	elem.innerHTML += "<br>Views: "+stream.channel.views+"<br>Followers: "+stream.channel.followers;
 	row.appendChild(elem);
     document.getElementById("results").appendChild(row);
   }
 }
+
+//sets up the buttons to go to the next or previous set of 10
+function setUpNav(links){
+  if(links.prev){
+	var prev = document.createElement("button");
+	prev.text = "prev";
+	prev.onclick = ajaxCall(links.prev);
+	document.getElementById("navigation").appendChild(prev);
+  }
+  if(links.next){
+	var next = document.createElement("button");
+	next.text = "next";
+	next.onclick = ajaxCall(links.next);
+	document.getElementById("navigation").appendChild(next);
+  }
+}
+
+//gets called if the api call does not work
 function error(result){
   document.getElementById("test").innerHTML += "failed";
 }
 
+//cleans out child elements
+function clean(elem){
+	var temp = elem;
+	  while (temp.firstChild) {
+        temp.removeChild(temp.firstChild);
+	  }
+}
